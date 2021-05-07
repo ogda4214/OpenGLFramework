3f32d2f598907ebb0b31d8de734707d214d0b905
ogda4214 <ogda4214@gmail.com>
2021-05-08 03:25:52 +0900

    シェーダコンパイル対応

--- OpenGLFrameWork/src/Application.cpp
+++ OpenGLFrameWork/src/Application.cpp
@@ -3,6 +3,49 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to complie " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    // シェーダプログラムリング処理
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -52,6 +95,31 @@ int main(void)
     // 定義された頂点属性を適用(インデックス)
     glEnableVertexAttribArray(0);
 
+    std::string vs =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fs =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    // シェーダコンパイル
+    unsigned int shader = createShader(vs,fs);
+    // シェーダプログラム使用
+    glUseProgram(shader);
+
     /* ウインドウループ */
     while (!glfwWindowShouldClose(window))
     {
@@ -68,6 +136,8 @@ int main(void)
         glfwPollEvents();
     }
 
+    // シェーダ解放
+    glDeleteShader(shader);
     /* glfw終了 */
     glfwTerminate();
 
