729fba5d396789e0bd6b2efaaaaf67013152f8c0
ogda4214 <ogda4214@gmail.com>
2022-01-04 07:30:17 +0900

    IMGUI表示対応

--- OpenGLFrameWork/src/Application.cpp
+++ OpenGLFrameWork/src/Application.cpp
@@ -14,6 +14,12 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -23,12 +29,14 @@ int main(void)
         return -1;
 
     /* glfw_openglパーション指定 */
+    const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* glfwウインドウ作成 */
     window = glfwCreateWindow(1024, 768, "Learn OpenGL", NULL, NULL);
+    //window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -66,6 +74,15 @@ int main(void)
             2,3,0
         };
 
+        // IMGUIコンテキスト生成
+        ImGui::CreateContext();
+        //ImGuiIO& io = ImGui::GetIO(); (void)io; io設定
+        ImGui::StyleColorsDark();
+        // プラントフォームとレンダラー設定(glfw+opengl3)
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        // ブレンド設定
         GLCall(glEnable(GL_BLEND))
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA))
 
@@ -83,15 +100,24 @@ int main(void)
         // インデックスバッファ生成
         IndexBuffer ib(indices, 6);
 
+        // モデル
+        glm::vec3 translate(0.0f, 1.0f, 0.0f);
+        // カメラ
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-2.0f, -1.0f, 0.0f));
+        // 正規投影マトリックス 4:3
+        glm::mat4 proj = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, -1.0f, 1.0f);
+
+
         // シェーダ生成
         Shader shader("res/shaders/Basic.shader");
         // シェーダ使用
         shader.Bind();
         // シェーダUniform使用
-        shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+        ImVec4 color = ImVec4(0.2f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniform4f("u_Color", color.x, color.y, color.z, color.w);
 
         // テクスチャ使用
-        Texture texture("res/texture/bg.png");
+        Texture texture("res/texture/awesomeface.png");
         texture.Bind(0);
         // シェーダUniform使用
         shader.SetUniform1i("u_Texture", 0);
@@ -103,17 +129,25 @@ int main(void)
         shader.Unbind();
         Renderer renderer;
 
-
-        float r = 0.0f;
-        float increment = 0.05f;
         /* ウインドウループ */
         while (!glfwWindowShouldClose(window))
         {
             // レンダラークリア
             renderer.Clear();
+
+            // IMGUIフレーム開始
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // MVPマトリックス計算
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translate);
+            glm::mat4 mvp = proj * view * model;
+
             // シェーダバインド
             shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniform4f("u_Color", color.x, color.y, color.z, color.w);
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             // 頂点配列バインド
             va.Bind();
@@ -122,16 +156,19 @@ int main(void)
             // レンダラー描画
             renderer.Draw(va, ib, shader);
 
-            if (r > 1.0f)
+            // IMGUI処理
             {
-                increment = -0.025f;
-            }
-            else if (r < 0.0f)
-            {
-                increment = 0.025f;
+                ImGui::Begin("Debug Menu");
+                ImGui::SliderFloat3("Translation", &translate.x, 4.0, -4.0f);
+                ImGui::ColorEdit3("Color", (float*)&color);
+                // FPS表示
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
             }
 
-            r += increment;
+            // IMGUIレンダリング
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             /* フロントバッファとバックバッファ交換 */
             glfwSwapBuffers(window);
@@ -140,6 +177,11 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+    // IMGUIクリア
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     /* glfw終了 */
     glfwTerminate();
